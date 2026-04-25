bd4a41d4f1731234c86db95410ab675bc8d93465
ghozali25 <ghozali.ahmd25@gmail.com>
2026-04-25 18:51:25 +0700

    Fix aria-hidden accessibility warning in modal

--- resources/views/components/overlays/modal.blade.php
+++ resources/views/components/overlays/modal.blade.php
@@ -31,11 +31,14 @@
     class="{{ $maxWidth }} mb-6 transform rounded-lg bg-white shadow-xl transition-all dark:bg-gray-800 sm:mx-auto w-full max-h-[90vh] overflow-y-auto"
     role="dialog"
     aria-modal="true"
-    x-trap.inert.noscroll="show" x-transition:enter="ease-out duration-300"
+    x-trap.inert.noscroll="show"
+    x-init="if(show) $nextTick(() => $el.focus())"
+    x-transition:enter="ease-out duration-300"
     x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200"
     x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-    x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+    x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    tabindex="-1">
     {{ $slot }}
   </div>
 </div>
